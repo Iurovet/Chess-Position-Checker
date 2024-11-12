0ea7c139d4f2d250c5a14a148b3ca8e33ad4ad7b
Iurovet <s4003879@student.rmit.edu.au>
2024-11-12 13:20:41 +1100

    Add printing of position to console.

--- PositionChecker.java
+++ PositionChecker.java
@@ -1,11 +1,31 @@
 import java.util.Arrays;
 
 public class PositionChecker {
+    private static void printBoard(char[][] position) {
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                System.out.print(" -");
+            }
+
+            System.out.println();
+
+            if (i < 8) {
+                for (int k = 0; k < 8; ++k) {
+                    System.out.print("|" + position[i][k]);
+                }
+
+                System.out.println("|");
+            }
+        }
+    }
+    
     public static void main(String[] args) {
         // Initialise chessboard position
         char[][] position = new char[8][8];
         for (char[] row : position) {
             Arrays.fill(row, '?');
         }
+
+        printBoard(position);
     }
 }
