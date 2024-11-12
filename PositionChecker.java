f5ebdc5571f939eabec8d06512105cdf238331b7
Iurovet <s4003879@student.rmit.edu.au>
2024-11-12 13:27:22 +1100

    Introduce prompts (very basic).

--- PositionChecker.java
+++ PositionChecker.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class PositionChecker {
     public static char[][] initialiseBoard() {
@@ -32,6 +33,16 @@ public class PositionChecker {
         // Initialise chessboard position
         char[][] position = initialiseBoard();
 
-        printBoard(position);
+        Scanner scnr = new Scanner(System.in);
+        String userInput = "";
+
+        while (!userInput.equalsIgnoreCase("q")) {
+            printBoard(position);
+
+            System.out.println("New command, q to quit");
+            userInput = scnr.nextLine();
+        }
+
+        scnr.close();
     }
 }
