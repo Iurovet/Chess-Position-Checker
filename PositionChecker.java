ae84d4657502b0c1b4823a0bb1dbc5e8c0a4a806
Iurovet <s4003879@student.rmit.edu.au>
2024-11-12 13:34:31 +1100

    Improve the look of the board.

--- PositionChecker.java
+++ PositionChecker.java
@@ -14,10 +14,10 @@ public class PositionChecker {
     public static void printBoard(char[][] position) {
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 8; ++j) {
-                System.out.print(" -");
+                System.out.print("--");
             }
 
-            System.out.println();
+            System.out.println("-");
 
             if (i < 8) {
                 for (int k = 0; k < 8; ++k) {
